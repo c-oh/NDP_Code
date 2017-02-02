c485599d7627fa035dabd4f610e537119c9a5ebe
hollythedev <papiniu@outlook.com>
2017-02-02 04:11:27 -0800

    Added comments

--- PHP_Assignemnt/functions.php
+++ PHP_Assignemnt/functions.php
@@ -18,13 +18,15 @@ print "Loading...\n";
 }
 // convert array to match structure in "correct-output.json"
 function convert_array_to_output_format($input_array) {
-  $output_array = 
+  #assigning a variable the value of an empty array
+  $output_array = array();
   print "Converting...\n";  
 
 
 }
 // save the array to file named "my-output.json" 
 function save_php_array_to_output_file($output_array) {
+  #This puts the contents of the var into the specified file
   file_put_contents('my-output.json', $output_array);
   print "Saving...\n";    
 
