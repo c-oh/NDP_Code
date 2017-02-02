483b66cb44f2d33a1a419e9a9d23e8aeccbfa588
hollythedev <papiniu@outlook.com>
2017-02-02 03:10:00 -0800

    Set up PHP section for coding

---
+++ PHP_Assignemnt/functions.php
@@ -0,0 +1,35 @@
+<?php
+
+########################
+###  EDIT THIS FILE  ###
+########################
+
+// read from "input.json" and return as an array
+function load_input_file_into_php_array() {
+  print "Loading...\n";
+
+
+}
+// convert array to match structure in "correct-output.json"
+function convert_array_to_output_format($input_array) {
+  print "Converting...\n";  
+
+
+}
+// save the array to file named "my-output.json" 
+function save_php_array_to_output_file($output_array) {
+  print "Saving...\n";    
+
+
+}
+
+########################################################################
+###  Note: Final "my-output.json" file should match structure of     ###
+###  "correct-output.json" - but, whitespace does NOT have to match. ###
+########################################################################
+
+########################################################################
+###  Tip - Look at these built-in PHP functions:                     ###
+###  json_encode, json_decode, file_put_contents, file_get_contents  ###
+########################################################################
+
